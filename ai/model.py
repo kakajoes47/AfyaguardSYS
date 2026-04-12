9b16d70ca43d07f0a56237eab48db9fb4d31eb86
kakajoes47 <kakajoes47@gmail.com>
2026-04-12 15:55:41 +0300

    Delete ai directory

--- ai/model.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import IsolationForest
-import joblib
-
-def train_model(data):
-    model = IsolationForest(contamination=0.05)
-    model.fit(data)
-    joblib.dump(model, "ai/ai_model.pkl")
-    return model
