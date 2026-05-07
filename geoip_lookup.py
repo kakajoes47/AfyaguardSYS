e3eaec272e7c77db3448319a61c62f1c71997f50
kakajoes47 <kakajoes47@gmail.com>
2026-05-07 13:46:29 +0300

    Update geoip_lookup.py

--- geoip_lookup.py
+++ geoip_lookup.py
@@ -1,31 +1,11 @@
-# geoip_lookup.py
-
 def get_location(ip):
-    """
-    Global Attack Source Intelligence
-
-    Maps suspicious source IPs
-    to their approximate attack origin location.
-    Demo version for lecturer presentation.
-
-    Real production version can later use:
-    ipinfo API
-    MaxMind GeoLite2
-    AbuseIPDB
-    VirusTotal intelligence feeds
-    """
-
     ip = str(ip)
-
-    sample_locations = {
+    locations = {
         "8.8.8.8": "United States",
         "185.143.223.12": "Russia",
         "45.77.12.89": "Germany",
-        "103.21.244.0": "Singapore",
-        "192.168.1.10": "Internal Hospital Network"
+        "192.168.1.10": "Internal Hospital Network",
+        "10.0.0.45": "Internal Hospital Network",
+        "172.16.5.20": "Internal Hospital Network"
     }
-
-    return sample_locations.get(
-        ip,
-        "Unknown Location"
-    )
+    return locations.get(ip, "Unknown Location")
