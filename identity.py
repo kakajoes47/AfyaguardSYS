cd557a38514b81c40677766a8e3b397abefa974a
kakajoes47 <kakajoes47@gmail.com>
2026-05-07 13:45:49 +0300

    Update identity.py

--- identity.py
+++ identity.py
@@ -1,36 +1,10 @@
-# identity.py
-
 def identify_device(ip):
-    """
-    Device Identity Monitoring Engine
-
-    Identifies the likely device type
-    based on IP address ranges
-    inside the hospital network.
-    """
-
     ip = str(ip)
-
-    # -----------------------------------
-    # Internal Hospital Devices
-    # -----------------------------------
     if ip.startswith("192.168"):
         return "Internal Hospital Device"
-
-    # -----------------------------------
-    # Medical Equipment
-    # -----------------------------------
     elif ip.startswith("10."):
-        return "Medical Equipment"
-
-    # -----------------------------------
-    # Administrative Systems
-    # -----------------------------------
+        return "Medical Equipment (IoMT)"
     elif ip.startswith("172."):
         return "Administrative System"
-
-    # -----------------------------------
-    # External Unknown Devices
-    # -----------------------------------
     else:
         return "External Unknown Device"
